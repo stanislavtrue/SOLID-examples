4b1ae550ec676cf791f894523af5c6cfd34560a5
Stanislav Pustovhar <flintbrawl66@gmail.com>
2025-12-12 19:44:20 +0100

    Update Program.cs

--- LSP/After/Program.cs
+++ LSP/After/Program.cs
@@ -1,25 +1,31 @@
 ﻿using System;
+
 abstract class Shape
 {
     public abstract double Area();
 }
+
 class Rectangle : Shape
 {
     public double Height { get; set; }
     public double Width { get; set; }
+    
     public override double Area()
     {
         return Height * Width;
     }
 }
+
 class Square : Shape
 {
     public double SideLength { get; set; }
+    
     public override double Area()
     {
         return SideLength * SideLength;
     }
 }
+
 class Program
 {
     static void Main(string[] args)
@@ -29,10 +35,12 @@ class Program
             Height = 5,
             Width = 4
         };
+        
         Shape square = new Square
         {
             SideLength = 5
         };
+        
         Console.WriteLine($"Area of the rectangle: {rectangle.Area()}");
         Console.WriteLine($"Area of the square: {square.Area()}");
     }
