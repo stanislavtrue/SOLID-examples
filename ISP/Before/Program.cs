b60264fbce007f710c49a600203ed497d9ae6c75
Stanislav Pustovhar <flintbrawl66@gmail.com>
2025-12-12 19:45:07 +0100

    Update Program.cs

--- ISP/Before/Program.cs
+++ ISP/Before/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Runtime.InteropServices;
+
 interface User
 {
     void ReadData();
@@ -7,27 +8,32 @@ interface User
     void BanUser();
     void DeleteSystem();
 }
+
 class SimpleUser : User
 {
     public void ReadData()
     {
         Console.WriteLine("User reads data.");
     }
+    
     public void WriteData()
     {
         Console.WriteLine("User writes data.");
     }
+    
     public void BanUser()
     {
         Console.WriteLine("User bans someone.");
         // But a simple user shouldn't have the right to ban other users
     }
+    
     public void DeleteSystem()
     {
         Console.WriteLine("User deletes the system.");
         // A simple user shouldn't have the right to delete the system
     }
 }
+
 class Program
 {
     static void Main(string[] args)
