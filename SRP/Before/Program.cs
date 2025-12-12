814c3d2bb38a66cbf502d62de5c4ba2fdf373c25
Stanislav Pustovhar <flintbrawl66@gmail.com>
2025-12-12 19:41:35 +0100

    Update Program.cs

--- SRP/Before/Program.cs
+++ SRP/Before/Program.cs
@@ -1,21 +1,26 @@
 ﻿using System;
 using System.IO;
+
 class Report
 {
     public string Text { get; set; }
+    
     public Report(string text)
     {
         Text = text;
     }
+    
     public void PrintReport()
     {
         Console.WriteLine($"Report: {Text}");
     }
+    
     public void SaveReport(string path)
     {
         File.WriteAllText(path, Text);
     }
 }
+
 class Program
 {
     static void Main(string[] args)
