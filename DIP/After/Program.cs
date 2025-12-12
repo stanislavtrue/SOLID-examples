a63ad7aec1e6095d0d103643fc235e5ab9e8c33f
Stanislav Pustovhar <flintbrawl66@gmail.com>
2025-12-12 19:46:50 +0100

    Update Program.cs

--- DIP/After/Program.cs
+++ DIP/After/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.IO.Compression;
+
 interface Database
 {
     void SaveData(string data);
 }
+
 class PostgreSqlDatabase : Database
 {
     public void SaveData(string data)
@@ -11,6 +13,7 @@ class PostgreSqlDatabase : Database
         Console.WriteLine($"Saving '{data}' into PostgreSQL database...");
     }
 }
+
 class MySqlDatabase : Database
 {
     public void SaveData(string data)
@@ -18,19 +21,23 @@ class MySqlDatabase : Database
         Console.WriteLine($"Saving '{data}' into MySQL database...");
     }
 }
+
 class DataService
 {
     private readonly Database _database;
+    
     public DataService(Database database)
     {
         _database = database;
     }
+    
     public void ProcessData(string data)
     {
         Console.WriteLine($"Processing: {data}");
         _database.SaveData(data);
     }
 }
+
 class Program
 {
     static void Main(string[] args)
